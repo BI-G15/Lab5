cb5593d9cf831e9a28a470c3e611f5f9cabc3cc6
Juan2707 <ju4nv3rgara@gmail.com>
2022-11-19 13:32:51 -0500

    limpieza y html por si acaso

--- file_util.py
+++ file_util.py
@@ -7,6 +7,11 @@ def cargar_datos(name):
 
 def guardar_datos(df, nombre):
     df.to_csv("/opt/airflow/data/" + nombre + ".csv" , encoding = 'latin1', sep=',', index=False)  
+
+def quitarErroneos(row):
+    if row != "Northwind" :
+      return "unknown"
+    return row
     
     
 def procesar_datos():
@@ -14,29 +19,43 @@ def procesar_datos():
     ## Dimension city
     city = pd.read_csv("http://bigdata-cluster4-01.virtual.uniandes.edu.co:50070/webhdfs/v1/user/monitorbi/datalakeBI/dimension_city.csv?op=OPEN&user.name=cursobi15", sep=',', encoding = 'latin1', index_col=False) # recuerden cambiar XX por el número de su grupo
     # To Do: Limpiar los datos y guardarlos
+    city=city.dropna()
+    #Se revisaron los datos numericos y demas columnas y no se encontraron inconsistencias en los datos
     guardar_datos(city, "dimension_city")
 
     ## Dimension Customer
     customer = pd.read_csv("http://bigdata-cluster4-01.virtual.uniandes.edu.co:50070/webhdfs/v1/user/monitorbi/datalakeBI/dimension_customer.csv?op=OPEN&user.name=cursobi15", sep=',', encoding = 'latin1', index_col=False)
     # To Do: Limpiar los datos y guardarlos
+    customer = customer.dropna()
+    #Se revisaron los datos numericos y demas columnas y no se encontraron inconsistencias en los datos
     guardar_datos(customer, "dimension_customer")
     
     ## Dimension Date
     date = pd.read_csv("http://bigdata-cluster4-01.virtual.uniandes.edu.co:50070/webhdfs/v1/user/monitorbi/datalakeBI/dimension_date.csv?op=OPEN&user.name=cursobi15", sep=',', encoding = 'latin1', index_col=False)
     # To Do: Limpiar los datos y guardarlos
+    #Los datos de date no contenian nulos
+    #Se revisaron los formatos de fechas, datos numericos y demas columnas y no se encontraron inconsistencias en los datos
     guardar_datos(date, "dimension_date")
 
     ## Dimension Employee
     employee = pd.read_csv("http://bigdata-cluster4-01.virtual.uniandes.edu.co:50070/webhdfs/v1/user/monitorbi/datalakeBI/dimension_employee.csv?op=OPEN&user.name=cursobi15", sep=',', encoding = 'latin1', index_col=False)
     # To Do: Limpiar los datos y guardarlos
+    employee = employee.dropna()
     guardar_datos(employee, "dimension_employee")
 
     ## Dimension Stock item
     stock_item = pd.read_csv("http://bigdata-cluster4-01.virtual.uniandes.edu.co:50070/webhdfs/v1/user/monitorbi/datalakeBI/dimension_stock_item.csv?op=OPEN&user.name=cursobi15", sep=',', encoding = 'latin1', index_col=False)
     # To Do: Limpiar los datos y guardarlos
+    #en stock_item la columna brand contenia muchos nulos, asi que se remplazaron los nulos con "unknown" para no borrar registros
+    stock_item["Brand"]=stock_item["Brand"].apply(quitarErroneos)
     guardar_datos(stock_item, "dimension_stock_item")
     
     ## Fact Table
     fact_order = pd.read_csv("http://bigdata-cluster4-01.virtual.uniandes.edu.co:50070/webhdfs/v1/user/monitorbi/datalakeBI/fact_order.csv?op=OPEN&user.name=cursobi15", sep=',', encoding = 'latin1', index_col=False)
     # To Do: Limpiar los datos y guardarlos
+    #Los datos se encuentran sin registros nulos
+    #Se revisaron los formatos de fechas, datos numericos y demas columnas y no se encontraron inconsistencias en los datos
     guardar_datos(fact_order, "fact_order")
+
+
+
