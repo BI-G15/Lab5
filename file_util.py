cb65feec02725cf801bd3c8f957257dbaf5ff24b
Jessica Robles <69654419+jessiRob@users.noreply.github.com>
2022-11-19 20:33:17 -0500

    Cambios en file_util

--- file_util.py
+++ file_util.py
@@ -13,6 +13,31 @@ def quitarErroneos(row):
       return "unknown"
     return row
     
+def quitarCaracteresEsp(row):
+    return ' '.join(filter(str.isalnum, row))
+
+def volverDecimal(row):
+    respuesta = row.replace(",",".")
+    if respuesta[0] == ".":
+        respuesta = "0" + respuesta 
+    return float(respuesta)
+
+# FUNCIONES DE LIMPIEZA
+
+def limpiarCustomer(df):
+    # columnas donde quiero remover caracteres:
+    columnas = ['Customer', 'Bill_To_Customer']
+    df[columnas] = df[columnas].replace({'\'':''}, regex=True)
+    return df
+
+def limpiarStock(df):
+    columnas = ['Tax_Rate', 'Unit_Price', 'Recommended_Retail_Price','Typical_Weight_Per_Unit']
+    df[columnas] = df[columnas].replace({',':'.'}, regex=True)
+    df['Tax_Rate'] = df['Tax_Rate'].apply(volverDecimal)
+    columnas2 = ['Stock_Item']
+    df[columnas2] = df[columnas2].replace({'\'':''}, regex=True)
+    return df
+
     
 def procesar_datos():
     
@@ -27,6 +52,7 @@ def procesar_datos():
     customer = pd.read_csv("http://bigdata-cluster4-01.virtual.uniandes.edu.co:50070/webhdfs/v1/user/monitorbi/datalakeBI/dimension_customer.csv?op=OPEN&user.name=cursobi15", sep=',', encoding = 'latin1', index_col=False)
     # To Do: Limpiar los datos y guardarlos
     customer = customer.dropna()
+    customer = limpiarCustomer(customer)
     #Se revisaron los datos numericos y demas columnas y no se encontraron inconsistencias en los datos
     guardar_datos(customer, "dimension_customer")
     
@@ -48,6 +74,9 @@ def procesar_datos():
     # To Do: Limpiar los datos y guardarlos
     #en stock_item la columna brand contenia muchos nulos, asi que se remplazaron los nulos con "unknown" para no borrar registros
     stock_item["Brand"]=stock_item["Brand"].apply(quitarErroneos)
+    stock_item = limpiarStock(stock_item)
+    columnas = ['Stock_Item']
+    stock_item[columnas] = stock_item[columnas].replace({'\'':''}, regex=True)
     guardar_datos(stock_item, "dimension_stock_item")
     
     ## Fact Table
